505198e8860250df78aa1fb65b4627c975b763ca
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2019-12-10 19:35:28 -0500

    Initial version of btl ice cream

---
+++ android/app/src/main/java/com/btldev/btl_ice_cream/MainActivity.java
@@ -0,0 +1,13 @@
+package com.btldev.btl_ice_cream;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
